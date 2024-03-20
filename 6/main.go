13f8093c59640a27788e44c9745775435cd317f6
Hugo Alexandro <hugo.a.dev@gmail.com>
2024-03-20 06:43:53 -0300

    Slices

--- 6/main.go
+++ 6/main.go
@@ -6,7 +6,7 @@ const a = "Hello, World"
 
 func main() {
 
-	s := []int{2, 4, 6, 8, 10}
+	s := []int{10, 20, 30, 40, 50, 60, 70, 80, 90, 100}
 	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
 
 	fmt.Printf("len=%d cap=%d %v\n", len(s[:0]), cap(s[:0]), s[:0])
