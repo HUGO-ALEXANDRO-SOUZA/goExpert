1ac595c751956b207262ff30ca7271dc0706ad7a
Hugo Alexandro <hugo.a.dev@gmail.com>
2024-04-08 05:35:54 -0300

    Preparando base de código

--- 1-fundamentos/20-generics/main.go
+++ 1-fundamentos/20-generics/main.go
@@ -22,7 +22,6 @@ func Compara[T comparable](a T, b T) bool {
 }
 
 func main() {
-
 	m := map[string]int{"Hugo": 1000, "Mariana": 2000, "Luara": 5000}
 	m2 := map[string]float64{"Hugo": 100.20, "Mariana": 200.3, "Luara": 300.0}
 	m3 := map[string]MyNumber{"Hugo": 1000, "Mariana": 2000, "Luara": 3000}
